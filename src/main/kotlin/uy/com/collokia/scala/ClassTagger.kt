0078c4377a2769d01573f65e5c738b15850082f8
Istvan Nagy <tyson925@gmail.com>
2016-06-10 14:24:21 +0200

    add movie examle to collaborate filtering

--- src/main/kotlin/uy/com/collokia/scala/ClassTagger.kt
+++ src/main/kotlin/uy/com/collokia/scala/ClassTagger.kt
@@ -1,5 +1,7 @@
 package uy.com.collokia.scala
 
 import scala.reflect.ClassTag
+import uy.com.collokia.scala.klass.ClassTagger
+
 
 public inline fun <reified T : Any> scalaClassTag(): ClassTag<T> = ClassTagger.scalaClassTag(T::class.java)
