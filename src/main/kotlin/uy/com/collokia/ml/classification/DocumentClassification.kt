f3bdc576b015aa72900e3322ffd58c5bf49d20c6
Istvan Nagy <tyson925@gmail.com>
2016-07-28 11:39:05 +0200

    document classification - first version

--- src/main/kotlin/uy/com/collokia/ml/classification/DocumentClassification.kt
+++ src/main/kotlin/uy/com/collokia/ml/classification/DocumentClassification.kt
@@ -2,17 +2,23 @@ package uy.com.collokia.ml.classification
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.apache.spark.SparkConf
+import org.apache.spark.api.java.JavaRDD
 import org.apache.spark.api.java.JavaSparkContext
 import org.apache.spark.ml.feature.*
 import org.apache.spark.mllib.linalg.SparseVector
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.RowFactory
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.sql.types.DataTypes
 import org.apache.spark.sql.types.Metadata
 import org.apache.spark.sql.types.StructField
 import org.apache.spark.sql.types.StructType
+import scala.Tuple2
+import uy.com.collokia.ml.rdf.DecisionTreeInSpark
 import java.io.File
 import java.io.Serializable
+import javax.xml.crypto.Data
 
 public data class ReutersDocument(val title: String?, var body: String?, val date: String,
                                   val topics: List<String>?, val places: List<String>?, val organisations: List<String>?, val id: Int) : Serializable
@@ -24,71 +30,145 @@ public class DocumentClassification() : Serializable {
     }
 
 
-    public fun createCorpus(jsc: JavaSparkContext) {
-        val corpusInRaw = jsc.textFile("./data/reuters/json/reuters.json")
+    public fun createCorpus(jsc: JavaSparkContext): JavaRDD<LabeledPoint> {
+        val corpusInRaw = jsc.textFile("./data/reuters/json/reuters.json").cache().repartition(8)
 
         val sqlContext = SQLContext(jsc)
+
+        val topCategories = listOf("ship", "grain", "money-fx", "corn", "trade", "crude", "earn", "wheat", "acq", "interest")
+        //val topCategories = listOf("earn", "acq")
 
         val corpus = corpusInRaw.map { line ->
             val doc = MAPPER.readValue(line, ReutersDocument::class.java)
             doc
         }.filter { doc ->
-            doc.topics != null
-        }.map { doc ->
-            RowFactory.create(doc.topics!!, doc.body!!)
+            doc.topics != null || doc.topics?.intersect(topCategories)?.isNotEmpty() ?: false
         }
 
-        val schema = StructType(arrayOf<StructField>(StructField("labels", DataTypes.createArrayType(DataTypes.StringType), false, Metadata.empty()), StructField(
-                "rawText", DataTypes.StringType, false, Metadata.empty())))
+        println("corpus size: " + corpus.count())
 
-        val textDataFrame = sqlContext.createDataFrame(corpus, schema)
+        val corpusRow = corpus.flatMap { doc ->
+            val topics = doc.topics?.intersect(topCategories) ?: listOf<String>()
+            topics.map { topic ->
+                RowFactory.create(topic, doc.body!!)
+            }
+        }
+
+        val schema = StructType(arrayOf<StructField>(StructField("category", DataTypes.StringType, true, Metadata.empty()), StructField(
+                "rawText", DataTypes.StringType, true, Metadata.empty())))
+
+        val textDataFrame = sqlContext.createDataFrame(corpusRow, schema)
+
+        val indexer = StringIndexer().setInputCol("category").setOutputCol("categoryIndex").fit(textDataFrame)
+        println(indexer.labels().joinToString("\t"))
+        println(indexer.extractParamMap())
+        println(indexer.params().joinToString("\t"))
+
+        val indexedTextDataFrame = indexer.transform(textDataFrame)
+
         val tokenizer = Tokenizer().setInputCol("rawText").setOutputCol("words")
-        val wordsDataFrame = tokenizer.transform(textDataFrame)
+        val wordsDataFrame = tokenizer.transform(indexedTextDataFrame)
 
         val remover = StopWordsRemover().setInputCol("words").setOutputCol("filteredWords")
 
         val filteredWordsDataFrame = remover.transform(wordsDataFrame)
 
-        val ngramTransformer = NGram().setInputCol("filteredWords").setOutputCol("ngrams")
+        val ngramTransformer = NGram().setInputCol("filteredWords").setOutputCol("ngrams").setN(4)
 
         val ngramsDataFrame = ngramTransformer.transform(filteredWordsDataFrame)
 
-        val numFeatures = 100000
+        val cvModel =  CountVectorizer()
+                .setInputCol("words")
+                .setOutputCol("features")
+                .setVocabSize(3)
+                .setMinDF(2.0)
+                .fit(wordsDataFrame)
+
+        val cvDataDrame = cvModel.transform(ngramsDataFrame)
+
+        val converter = IndexToString()
+                .setInputCol("categoryIndex")
+                .setOutputCol("originalCategory")
+        val converted = converter.transform(cvDataDrame)
+
+
+        val featureData = converted.select("features", "categoryIndex", "originalCategory")
+
+        for (r in featureData.take(3)) {
+
+            val features = r.getAs<SparseVector>(0)
+            val label = r.getDouble(1)
+            val original = r.getString(2)
+            System.out.println(features)
+            println(label)
+            println(original)
+        }
+
+        val labeledDataPoints = featureData.toJavaRDD().map({ feature ->
+            val features = feature.getAs<SparseVector>(0)
+            val label = feature.getDouble(1)
+//            println(label)
+            LabeledPoint(label, features)
+        })
+
+        println("number of data: " + labeledDataPoints.count())
+
+        val labelStat = featureData.select("originalCategory").javaRDD().mapToPair { label ->
+            Tuple2(label.getString(0), 1L)
+        }.reduceByKey { a, b -> a + b }
+
+        println(labelStat.collectAsMap())
+
+        return labeledDataPoints
+    }
+
+    public fun setTfIdf(corpus: DataFrame) {
+        val numFeatures = 2000
 
         val hashingTF = HashingTF().setInputCol("ngrams")
                 .setOutputCol("tfFeatures")
                 .setNumFeatures(numFeatures)
 
-        val tfFeatures = hashingTF.transform(ngramsDataFrame)
+        val tfFeatures = hashingTF.transform(corpus)
 
-        val idf = IDF().setInputCol("tfFeatures").setOutputCol("idfFeatures")
+        val idfModel = setTfIdfModel(tfFeatures)
 
-        val idfModel = idf.fit(tfFeatures)
+        val rescaledData = idfModel.transform(tfFeatures)
+    }
+
+    public fun applyTfIdf(corpus: DataFrame, hashingTF: HashingTF, idfModel: IDFModel): DataFrame {
+        val tfFeatures = hashingTF.transform(corpus)
 
         val rescaledData = idfModel.transform(tfFeatures)
 
-        for (r in rescaledData.select("idfFeatures", "labels").take(3)) {
-            val features = r.getAs<SparseVector>(0)
-            val label = r.getList<String>(1)
-            System.out.println(features)
-            println(label)
-        }
+        return rescaledData
+    }
+
+    public fun setTfIdfModel(corpus: DataFrame): IDFModel {
+        val idf = IDF().setInputCol("tfFeatures").setOutputCol("idfFeatures")
+
+        val idfModel = idf.fit(corpus)
+
+        return idfModel
     }
 
 
     public fun readJson() {
+        var index = 0
+        File("./data/reuters/json/reuters.json").bufferedWriter().use { writer ->
+            val time = File("./data/reuters/").listFiles().filter { file -> file.name.endsWith(".json") }.forEach { file ->
+                val jsons = file.readLines().joinToString("\n").split("},").toMutableList()
+                jsons[0] = jsons[0].substring(1)
+                jsons[jsons.lastIndex] = jsons[jsons.lastIndex].substringBeforeLast("]")
 
-        val time = File("./data/reuters/").listFiles().filter { file -> file.name.endsWith(".json") }.forEach { file ->
-            val jsons = file.readLines().joinToString("\n").split("},").toMutableList()
-            jsons[0] = jsons[0].substring(1)
-            jsons[jsons.lastIndex] = jsons[jsons.lastIndex].substringBeforeLast("]")
 
-            File("./data/reuters/json/reuters.json").bufferedWriter().use { writer ->
+
                 jsons.forEach { json ->
                     //println(cleanJson(json))
                     val reutersDoc = MAPPER.readValue(cleanJson(json), ReutersDocument::class.java)
 
                     reutersDoc.body?.let { body ->
+                        println(index++)
                         reutersDoc.body = reutersDoc.body?.replace("\n", "")
                         writer.write(MAPPER.writeValueAsString(reutersDoc) + "\n")
                     }
@@ -106,11 +186,21 @@ public class DocumentClassification() : Serializable {
     }
 
     public fun runOnSpark() {
-        val sparkConf = SparkConf().setAppName("reutersTest").setMaster("local[6]")
+        val sparkConf = SparkConf().setAppName("reutersTest").setMaster("local[8]")
 
         val jsc = JavaSparkContext(sparkConf)
 
-        createCorpus(jsc)
+        val data = createCorpus(jsc)
+        val (trainData, cvData, testData) = data.randomSplit(doubleArrayOf(0.8, 0.1, 0.1))
+        trainData.cache()
+        cvData.cache()
+        testData.cache()
+
+        val dt = DecisionTreeInSpark()
+        //dt.evaluateForest(data)
+        dt.evaluate(trainData, cvData, testData, 10)
+        //println(dt.classProbabilities(trainData).joinToString("\n"))
+        //dt.simpleDecisionTree(trainData,testData,10)
 
     }
 
--- src/main/kotlin/uy/com/collokia/ml/rdf/DecisionTreeInSpark.kt
+++ src/main/kotlin/uy/com/collokia/ml/rdf/DecisionTreeInSpark.kt
@@ -10,23 +10,24 @@ import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.mllib.tree.DecisionTree
 import org.apache.spark.mllib.tree.RandomForest
 import org.apache.spark.mllib.tree.model.DecisionTreeModel
+import org.apache.spark.mllib.tree.model.RandomForestModel
 import scala.Tuple2
 import java.io.Serializable
 
 
 public class DecisionTreeInSpark() : Serializable {
 
-    public fun simpleDecisionTree(trainData: JavaRDD<LabeledPoint>, cvData: JavaRDD<LabeledPoint>) {
+    public fun simpleDecisionTree(trainData: JavaRDD<LabeledPoint>, cvData: JavaRDD<LabeledPoint>, numClasses : Int) {
 
         // Build a simple default DecisionTreeModel
-        val model = DecisionTree.trainClassifier(trainData, 7, mapOf<Int, Int>(), "gini", 4, 100)
+        val model = DecisionTree.trainClassifier(trainData, numClasses, mapOf<Int, Int>(), "gini", 4, 100)
 
         val metrics = getMetrics(model, cvData)
 
         println(metrics.confusionMatrix())
         println(metrics.precision())
 
-        val res = (0..6).map({ category ->
+        val res = (0..numClasses-1).map({ category ->
             "${category}\tprecision:\t${metrics.precision(category.toDouble())}, recall:\t${metrics.recall(category.toDouble())}, F-measure:\t${metrics.fMeasure(category.toDouble())}"
 
         }).joinToString("\n")
@@ -62,13 +63,13 @@ public class DecisionTreeInSpark() : Serializable {
         return counts.map({ it -> it.toDouble() / counts.sum() }).toDoubleArray()
     }
 
-    public fun evaluate(trainData: JavaRDD<LabeledPoint>, cvData: JavaRDD<LabeledPoint>, testData: JavaRDD<LabeledPoint>) {
+    public fun evaluate(trainData: JavaRDD<LabeledPoint>, cvData: JavaRDD<LabeledPoint>, testData: JavaRDD<LabeledPoint>,numClasses : Int) {
         val evaluations =
                 listOf("gini", "entropy").flatMap { impurity ->
                     intArrayOf(1, 20).flatMap { depth ->
                         intArrayOf(10, 300).map { bins ->
                             val model = DecisionTree.trainClassifier(
-                                    trainData, 7, mapOf<Int, Int>(), impurity, depth, bins)
+                                    trainData, numClasses, mapOf<Int, Int>(), impurity, depth, bins)
                             val accuracy = getMetrics(model, cvData).precision()
                             Tuple2(Triple(impurity, depth, bins), accuracy)
                         }
@@ -78,7 +79,7 @@ public class DecisionTreeInSpark() : Serializable {
         println(evaluations.sortedBy({ it -> it._2 }).reversed().joinToString("\n"))
 
         val model = DecisionTree.trainClassifier(
-                trainData.union(cvData), 7, mapOf<Int, Int>(), "entropy", 20, 300)
+                trainData.union(cvData), numClasses, mapOf<Int, Int>(), "entropy", 20, 300)
         println(getMetrics(model, testData).precision())
         println(getMetrics(model, trainData.union(cvData)).precision())
     }
@@ -141,25 +142,31 @@ public class DecisionTreeInSpark() : Serializable {
     public fun evaluateForest(rawData: JavaRDD<String>) {
         val data = unencodeOneHot(rawData)
 
+        val forest = evaluateForest(data)
+        val input = "2709,125,28,67,23,3224,253,207,61,6094,0,29"
+        val vector = Vectors.dense(input.split(',').map({ it -> it.toDouble() }).toDoubleArray())
+        println(forest.predict(vector))
+    }
+
+    public fun evaluateForest(data: JavaRDD<LabeledPoint>): RandomForestModel {
         val (trainData, cvData) = data.randomSplit(doubleArrayOf(0.9, 0.1))
         trainData.cache()
         cvData.cache()
 
-        val forest = RandomForest.trainClassifier(
-                trainData, 7, mapOf(10 to 4, 11 to 40), 20, "auto", "entropy", 30, 300,12)
+        val forestModel = RandomForest.trainClassifier(trainData, 7, mapOf(10 to 4, 11 to 40), 20, "auto", "entropy", 30, 300, 12)
+
+        trainData.unpersist()
 
         val predictionsAndLabels = cvData.map({ example ->
-            Tuple2(forest.predict(example.features()) as Any, example.label() as Any)
+            Tuple2(forestModel.predict(example.features()) as Any, example.label() as Any)
         })
         println(MulticlassMetrics(predictionsAndLabels.rdd()).precision())
 
-        val input = "2709,125,28,67,23,3224,253,207,61,6094,0,29"
-        val vector = Vectors.dense(input.split(',').map({it ->it.toDouble()}).toDoubleArray())
-        println(forest.predict(vector))
+        return forestModel
     }
 
 
-    public fun RunRDF() {
+    public fun runRDF() {
         val sparkConf = SparkConf().setAppName("DecisionTree").setMaster("local[6]")
 
         val jsc = JavaSparkContext(sparkConf)
@@ -167,7 +174,7 @@ public class DecisionTreeInSpark() : Serializable {
         val rawData = jsc.textFile("./data/DT/covtype.data.gz")
 
         val data = rawData.map { line ->
-            val values = line.split(',').map({value ->value.toDouble()})
+            val values = line.split(',').map({ value -> value.toDouble() })
             val featureVector = Vectors.dense(values.toDoubleArray())
             val label = values.last() - 1
             LabeledPoint(label, featureVector)
@@ -182,7 +189,7 @@ public class DecisionTreeInSpark() : Serializable {
         //randomClassifier(trainData, cvData)
         //evaluate(trainData, cvData, testData)
         evaluateCategorical(rawData)
-        //evaluateForest(rawData)
+        evaluateForest(rawData)
 
         trainData.unpersist()
         cvData.unpersist()
@@ -196,6 +203,6 @@ public class DecisionTreeInSpark() : Serializable {
 fun main(args: Array<String>) {
     BasicConfigurator.configure()
     val decisionTree = DecisionTreeInSpark()
-    decisionTree.RunRDF()
+    decisionTree.runRDF()
 }
 
